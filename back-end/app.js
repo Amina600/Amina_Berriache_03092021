3206140505d5235c99b79eae9c1aeaf921887c12
Amina600 <aminaharaoui19@gmail.com>
2021-09-03 15:32:26 +0200

    installation de mangooseDB et le connecter avec l'api

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,26 @@
-//Importer express
+//Importer express, mangoose
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+// connecter l'api avec mangooseDB
+mongoose.connect('mongodb+srv://amoza:dayfutur1@cluster0.fns7z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Middlewares reçoivent des objets request et response et next()
-//Fonction next() permet à chaque middleware de passer l'exécution au middleware suivan
+//Fonction next() permet à chaque middleware de passer l'exécution au middleware suivant
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.use((req, res, next) => {
     console.log('Requête reçue !');
     next();
